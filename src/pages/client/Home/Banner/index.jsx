f14cfab67dd5b777d6e75bd1551ef801c98ded78
anh <panh9151@gmail.com>
2024-03-15 19:31:47 +0700

    a

--- src/pages/client/Home/Banner/index.jsx
+++ src/pages/client/Home/Banner/index.jsx
@@ -1,10 +1,12 @@
 import classes from "./Banner.module.scss";
 
 import banner from "../../../../assets/images/home/banner.png";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const navigate = useNavigate();
   return (
-    <div className={classes.banner}>
+    <div className={classes.banner} onClick={() => navigate("/shop")}>
       <img src={banner} alt="" />
     </div>
   );
